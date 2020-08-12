9109e0da4f4eb89f28b2c599be6c0117ed96ed68
Jimmy Lin <b00502013@gmail.com>
2020-08-13 02:07:59 +0800

    Update tests

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,6 @@
-# the default import 
-
-# my import
 from rest_framework import serializers
-
-# my import which may grow during adding new models
 from crm.models import Person
 from crm.models import Device
-
-# my code
 
 
 class PersonSerializer(serializers.ModelSerializer):
@@ -15,18 +8,11 @@ class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = (
-            'url',
             'id',
-            'code',
+            'uuid',
             'name',
-            'tax_id',
             'phone_number',
-            'address',
-            'registration_time',
-            'expiration_time',
             'remark',
-            'parent',
-            'related_salesperson',
         )
         extra_kwargs = {
             'url': {'view_name': 'api:crm:person-detail'}
@@ -38,19 +24,13 @@ class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
         fields = (
-            'url',
             'id',
-            'code',
+            'uuid',
             'name',
-            'power_state',
             'os',
             'cpu',
             'ram',
-            'registration_time',
-            'expiration_time',
-            'vmware_tool_state',
             'remark',
-            'parent',
             'customer',
         )
         extra_kwargs = {
